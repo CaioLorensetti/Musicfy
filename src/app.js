d0e5ff329cebb0c001dc262e9d8e5a10498c9ad8
Lorensetti, Caio <caiolorensetti@gmail.com>
2022-07-11 00:01:54 -0400

    Entregando Playlists em arquivo CSV

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-
 const database = require('./database')
+const json2csv = require('json2csv')
 
 const app = express()
 const port = 3000
@@ -9,7 +9,11 @@ const port = 3000
 app.get('/playlists', (request, response) => {
 
   database.getAllPlaylists(function (data) {
-    response.json(data)  
+    const fields = Object.keys(data[0])
+    const parser = new json2csv.Parser({fields})
+    const csv = parser.parse(data)
+    response.attachment('playlists.csv')
+    response.status(200).send(csv)
   })
 
 })
